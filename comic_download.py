802ce949e600bd6052928ba2c7e04632cf225523
Michael Hsiu <xypcx@hotmail.com>
2025-06-02 13:20:13 +0800

    更新comic_download.py

--- comic_download.py
+++ comic_download.py
@@ -7,7 +7,7 @@ base_url = "https://m.g-mh.org"
 api_base_url = "https://api-get-v2.mgsearcher.com"
 img_base_url = "https://f40-1-4.g-mh.online"
 webhook_url = os.getenv('discord_webhook')
-headers = {
+base_headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
     'Referer': 'https://m.g-mh.org/'
 }
@@ -21,16 +21,16 @@ def download_comic(comic_mid):
     """下载整部漫画，并合并结果"""
     send_to_webhook("任务开始")
     try:
-        response = requests.get(
-        'https://api-get-v2.mgsearcher.com/api/manga/get?mid=28197&mode=all',
-        headers=headers
-    )
+        session = requests.Session()
+        session.headers.update(base_headers)
+        response = session.get('https://api-get-v2.mgsearcher.com/api/manga/get?mid=28197&mode=all')
+       
         response.raise_for_status()
         data = response.json()
         chapters = data['data']['chapters']
 
         # 合并并保存结果
-
+        send_to_webhook(f"任务成功！")
 
     except requests.exceptions.RequestException as e:
         send_to_webhook(f"发生错误：{e}")
