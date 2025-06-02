640b0f94595bf8647907931f0fa1ac0a03db5d53
Michael Hsiu <xypcx@hotmail.com>
2025-06-02 12:35:07 +0800

    更新comic_download.py

--- comic_download.py
+++ comic_download.py
@@ -2,14 +2,8 @@
 import requests
 
 # 全局变量
-base_url = "https://m.g-mh.org"
-api_base_url = "https://api-get-v2.mgsearcher.com"
-img_base_url = "https://f40-1-4.g-mh.online"
+
 webhook_url = os.getenv('discord_webhook')
-base_headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-    'Referer': 'https://m.g-mh.org/'
-}
 
 def send_to_webhook(message):
 
