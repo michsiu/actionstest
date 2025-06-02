3f2c7322bfff394f40e4bd9d5871194d75e2e9be
Michael Hsiu <xypcx@hotmail.com>
2025-06-02 13:21:47 +0800

    更新comic_download.py

--- comic_download.py
+++ comic_download.py
@@ -7,9 +7,13 @@ base_url = "https://m.g-mh.org"
 api_base_url = "https://api-get-v2.mgsearcher.com"
 img_base_url = "https://f40-1-4.g-mh.online"
 webhook_url = os.getenv('discord_webhook')
-base_headers = {
+base_ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-    'Referer': 'https://m.g-mh.org/'
+    'Referer': 'https://m.g-mh.org/',
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Origin': 'https://m.g-mh.org',
+    'Connection': 'keep-alive',
 }
 
 def send_to_webhook(message):
