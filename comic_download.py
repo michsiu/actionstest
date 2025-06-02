1691db796444a74c88fec877a12d5f9eedba0afa
Michael Hsiu <xypcx@hotmail.com>
2025-06-02 12:28:18 +0800

    更新comic_download.py

--- comic_download.py
+++ comic_download.py
@@ -4,12 +4,9 @@ import os
 webhook_url = os.getenv('discord_webhook')
 
 def send_to_webhook(message):
-    if webhook_url is None:
-        print("Webhook URL 未设置！")
-        return False
+
     payload = {"content": message}
     response = requests.post(webhook_url, json=payload, headers={'Content-Type': 'application/json'})
-    return response.status_code == 200  # 返回是否发送成功
 
 if __name__ == "__main__":
     with open('comic_download_task.txt', 'r', encoding='utf-8') as file:
